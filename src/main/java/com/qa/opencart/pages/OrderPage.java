5ecf7506d966dc74028d6348161308e454f7633c
Gaurav <gauravtiwari7@gmail.com>
2023-03-19 01:11:54 +0530

    updated ordr page as per feedback

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -5,8 +5,13 @@ import org.openqa.selenium.By;
 public class OrderPage {
 	
 	By loc=By.id("order");
+	By price=By.id("price");
+
 	public void getorder() {
 		System.out.println("get order");
+	}
+	public void getprice() {
+		System.out.println("get price");
 	}
 	
 
