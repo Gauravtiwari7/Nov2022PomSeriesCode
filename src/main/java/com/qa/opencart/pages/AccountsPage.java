10d962dc70a0ed8ac8e1459571f3df06c20e08d4
Gaurav <gauravtiwari7@gmail.com>
2023-03-20 00:34:22 +0530

    updated accounts page class

--- src/main/java/com/qa/opencart/pages/AccountsPage.java
+++ src/main/java/com/qa/opencart/pages/AccountsPage.java
@@ -28,7 +28,7 @@ public class AccountsPage {
 	public String getAccPageTitle() {
 		String title=eleUtil.waitForTitleIsAndFetch(AppConstants.DEFAULT_SHORT_TIME_OUT, AppConstants.ACCOUNTS_PAGE_TITLE_VALUE);
 		//String title=driver.getTitle();
-		System.out.println("Acc page title  : "+title);
+		System.out.println("Acc page title is  : "+title);
 		return title;
 	}
 	public String getAccPageURL() {
